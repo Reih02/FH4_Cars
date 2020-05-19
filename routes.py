5071b81f889b80d195171b9ec3b44c66d42ef4c5
Reilly Haskins <38972253+Reih02@users.noreply.github.com>
2020-05-19 22:22:37 +1200

    added navbar and some text for home page

--- routes.py
+++ routes.py
@@ -3,9 +3,11 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html", page_title="WELCOME TO FH4 CARS")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=1111)
