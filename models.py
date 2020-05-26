b11b08b3743e0c2777a651b4fd78e6e7789ac4bf
Reih02 <reillyhaskins@gmail.com>
2020-05-26 12:45:52 +1200

    got names of cars to show up on list_cars.html, but having problems with the images displaying

---
+++ models.py
@@ -0,0 +1,14 @@
+from routes import db
+
+
+class Car(db.Model):
+    __tablename__ = 'Car'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.Text(50), nullable=False)
+    horsepower = db.Column(db.Text, nullable=False)
+    cost = db.Column(db.Text, nullable=False)
+    wheeldrive = db.Column(db.Text, nullable=False)
+    weight = db.Column(db.Text, nullable=False)
+    image = db.Column(db.Text)
+    manufacturerid = db.Column(db.Text, db.ForeignKey('Manufacturer.id'))
--- routes.py
+++ routes.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import models
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FH4_cars.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def home():
     return render_template("home.html", page_title="WELCOME TO FH4 CARS")
+
+
+@app.route('/cars')
+def cars():
+    cars = models.Car.query.all()
+    return render_template('list_cars.html', page_title="CAR LIST",
+                           cars=cars)
 
 
 if __name__ == "__main__":
