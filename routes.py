bfdb488f0eec503879b1be46710b82a94bb27dd5
Reih02 <reillyhaskins@gmail.com>
2020-06-13 22:47:49 +1200

    Revert "began making login page for site"
    This reverts commit cf50fb0a64bf33c25290321b0b753e71feaed6b8.

--- routes.py
+++ routes.py
@@ -1,22 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from app import app
-from app.forms import LoginForm
 import models
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'my-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FH4_cars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/')
