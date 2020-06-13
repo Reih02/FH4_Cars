96fdc62a45de1e8a26d5eba8e4b4241f23fbe56b
Reih02 <reillyhaskins@gmail.com>
2020-06-13 23:22:08 +1200

    finished login page, starting on signup page

--- routes.py
+++ routes.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 import models
 from forms import LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret'
+app.config['SECRET_KEY'] = 'reilly_is_cool'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///FH4_cars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
 
