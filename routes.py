189e53ba788f906e020869552830a25943914fa0
Reih02 <reillyhaskins@gmail.com>
2020-09-04 08:38:35 +1200

    commented 500 errorhandler route

--- routes.py
+++ routes.py
@@ -285,6 +285,7 @@ def page_not_found(e):
     return render_template("404.html")
 
 
+# error handler for a 500 error (returns 500.html instead of standard 500 page)
 @app.errorhandler(500)
 def internal_server(e):
     return render_template("500.html")
