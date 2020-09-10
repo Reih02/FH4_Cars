aa44bcf15e29f473f0e7035f83b4ec33a10a7d75
Reih02 <reillyhaskins@gmail.com>
2020-09-11 09:59:08 +1200

    Revert "fixed problem with afore-mentioned solution"
    This reverts commit a11e3cf84974b3dc672207fd1db70a90298e62c0.

--- static/js.js
+++ static/js.js
@@ -1,6 +1,11 @@
-// jQuery functionality for all forms (except delete), to stop double submits
+<script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
 
-// disable submit button after first click to avoid spam clicks
+  // jQuery functionality for all forms (except delete), to stop double submits
+
+  // disable submit button after first click to avoid spam clicks
   $(document).ready(function() {
       // define fields as variables
       var submit_button = $('input#submit');
