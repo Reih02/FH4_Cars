525d60e00c14577603fd1320ad37c68cf101562d
Reih02 <reillyhaskins@gmail.com>
2020-09-10 14:04:58 +1200

    got more efficient solution for double-registration of signup form from bob

--- run.bat
+++ run.bat
@@ -1,7 +1,12 @@
 @echo off
 echo Installing required packages
 @pip install -r requirements.txt
+@C:
+@cd C:\Python36\Scripts\
+@pip install -r E:\13DTP\FH4_Cars\requirements.txt
 echo Finished installing any required packages
 @python routes.py
+@cd C:\Python36\
+@python E:\13DTP\FH4_Cars\routes.py
 echo Program crashed/quit
 pause()
