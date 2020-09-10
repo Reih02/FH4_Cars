f03536208796d616cbdba4197390ce15f70e2ee6
Reih02 <reillyhaskins@gmail.com>
2020-09-11 10:16:17 +1200

    implemented fix for double sign up submission again

--- static/js.js
+++ static/js.js
@@ -1,8 +1,3 @@
-<script
-  src="https://code.jquery.com/jquery-3.5.1.js"
-  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script>
-
   // jQuery functionality for all forms (except delete), to stop double submits
 
   // disable submit button after first click to avoid spam clicks
